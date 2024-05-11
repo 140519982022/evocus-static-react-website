9a39a45562a4e09c4796f228953d62f8abaa3b84
nikita <you@excnikitakose1414@gmail.com>
2024-05-11 17:18:58 +0530

    adjust about us

--- src/components/ImgContentSection2.jsx
+++ src/components/ImgContentSection2.jsx
@@ -8,36 +8,38 @@ export default function ImgContentSection2() {
 
             <div className=' container-fluid bg-body-secondary my-5'>
 
-                <div className='row'>
-                    <div className='col-md-6 ps-5'>
+                <div className="container max-auto">
+                    <div className='row '>
+                        <div className='col-md-6 ps-5'>
 
 
-                        <div className='h1 pt-5'>
-                        Expert Cloud Services
+                            <div className='h1 pt-5'>
+                                Expert Cloud Services
 
 
+                            </div>
+
+                            <div className='text-secondary py-4 lh-lg' style={{ fontSize: '20px' }}>
+                                Our Experienced Architects Provide Personalized Design Solutions That <br /> Turn Your Dreams Into Reality.
+
+
+
+
+
+                            </div>
+
+                            <div className=' pe-5'>
+                                <Button variant="primary" size="lg" className='my-3'>LEARN MORE ABOUT CLOUD SYSTEMS</Button>
+                            </div>
                         </div>
 
-                        <div className='text-secondary py-4 lh-lg' style={{ fontSize: '20px' }}>
-                        Our Experienced Architects Provide Personalized Design Solutions That <br /> Turn Your Dreams Into Reality.
+                        <div className='col-md-6 d-flex flex-row-reverse'>
 
 
-
-
-
+                            <img src={contentImg} alt="" className='w-75 ' />
                         </div>
 
-                        <div className=' pe-5'>
-                            <Button variant="primary" size="lg" className='my-3'>LEARN MORE ABOUT CLOUD SYSTEMS</Button>
-                        </div>
                     </div>
-
-                    <div className='col-md-6 d-flex flex-row-reverse'>
-
-
-                        <img src={contentImg} alt="" className='w-75 ' />
-                    </div>
-
                 </div>
 
 
