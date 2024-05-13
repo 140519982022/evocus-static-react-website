5e8ff5ff41034b94d59cceaf1a3fa3592b62bf8f
nikita <you@excnikitakose1414@gmail.com>
2024-05-13 12:09:01 +0530

    remove dots arrows from carousel

--- src/components/ContentCarouselSection.jsx
+++ src/components/ContentCarouselSection.jsx
@@ -7,47 +7,33 @@ import slider4 from '../assets/images/slider4.avif';
 
 import Button from 'react-bootstrap/Button';
 
-
-// import hmm2 from '../assets/images/slider3.avif';
-
 export default function ContentCarouselSection() {
-
     const images = [
         slider1,
         slider2,
         slider3,
         slider2,
         slider4
-
-
     ];
 
     return (
-
         <>
             <div className=' container-fluid bg-body-secondary py-5 my-5 position-relative'>
-
                 <div className='container d-flex flex-column justify-content-between'>
                     <div className='row'>
-
                         <div className='col-md-11 m-auto'>
-
-
                             <div className='h1 pt-5'>
                                 Trusted by over 100+ of the world's most innovative companies
                             </div>
-
                             <div className='text-secondary py-4 text-center' style={{ fontSize: '20px' }}>
                                 From consulting and strategy development to implementation and support, our comprehensive services <br />
                                 can help your business thrive.
                             </div>
                         </div>
                     </div>
-
-
                     <Row className='row py-5'>
                         <Col>
-                            <Carousel interval={2000} pause={false} wrap={true} showIndicators={false}>
+                            <Carousel interval={2000} pause={false} wrap={true} indicators={false} prevIcon={null} nextIcon={null}>
                                 {images.map((chunk, index) => (
                                     <Carousel.Item key={index}>
                                         <Row>
@@ -62,16 +48,11 @@ export default function ContentCarouselSection() {
                             </Carousel>
                         </Col>
                     </Row>
-
-
                 </div>
                 <div className='position-absolute top-100 start-50 translate-middle'>
-                            <Button variant="primary" size="" className=''>LEARN MORE ABOUT US</Button>
-                        </div>                        
-
+                    <Button variant="primary" size="" className=''>LEARN MORE ABOUT US</Button>
+                </div>
             </div>
-
-
         </>
     )
 }
